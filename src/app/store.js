117d905b35b4341f599b7997c96bd565069e9e34
NKP1229 <113551880+NKP1229@users.noreply.github.com>
2025-04-07 16:08:09 -0400

    reducers, store

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import transactions from "../features/transactions/transactionsSlice";
 // TODO: Configure the store to use the reducer from the transactions slice.
-const store = configureStore();
+export const store = configureStore({
+    reducer: {
+        transactions,
+    }
+});
 
-export default store;
+//export default store;
